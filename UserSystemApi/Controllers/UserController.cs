52036c9115f4e205aa289a92ba80c8532d30c13d
unknown <ricanteq@gmail.com>
2023-04-11 13:10:20 +0100

    initial commit

---
+++ UserSystemApi/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc;
+using UserSystemApi.Models;
+
+
+namespace UserSystemApi.Services;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly UserService _userService;
+
+    public UserController(UserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost]
+    public IActionResult Create(User user)
+    {
+        if (string.IsNullOrWhiteSpace(user.FirstName)) return BadRequest("Firstname cannot be empty");
+        if (user.LastName == null) return BadRequest("Lastname cannot be empty");
+        if (user.Email == null) return BadRequest("Email cannot be empty");
+        if (user.Password == null) return BadRequest("Password cannot be empty");
+
+        return Ok(_userService.CreateUser(user));
+    }
+
+
+    [HttpGet]
+    public ActionResult<List<User>> GetAllUsers()
+    {
+        return _userService.GetAllUsers();
+    }
+    
+    
+    [HttpGet("{id}")]
+    public ActionResult<User> GetUserById(int id)
+    {
+        var user = _userService.GetUserById(id);
+        if (user == null) return NotFound();
+
+        return user;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, User updatedUser)
+    {
+        var userToUpdate = _userService.GetUserById(id);
+        if (userToUpdate == null) return NotFound();
+
+        userToUpdate.FirstName = updatedUser.FirstName ?? userToUpdate.FirstName;
+        userToUpdate.LastName = updatedUser.LastName;
+        userToUpdate.Email = updatedUser.Email;
+        userToUpdate.Password = updatedUser.Password;
+
+        return Ok(userToUpdate);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteUser(int id)
+    {
+        _userService.DeleteUser(id);
+        return NoContent();
+    }
+}
---
+++ UserSystemApi/Services/UserService.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BCrypt.Net;
+using UserSystemApi.Models;
+
+namespace UserSystemApi.Services
+{
+    public class UserService
+    {
+        private static readonly List<User> _userList = new();
+
+        public async Task<User> CreateUser(User user)
+        {
+            user.Id = _userList.Count + 1;
+            var newUser = new User
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Password = HashPassword(user.Password)
+            };
+            _userList.Add(newUser);
+            return await Task.FromResult(newUser);
+        }
+
+        private string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return _userList;
+        }
+
+        public User GetUserById(int id)
+        {
+            return _userList.FirstOrDefault(u => u.Id == id);
+        }
+
+        public async Task<bool> VerifyPassword(User user, string password)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, user.Password);
+        }
+
+        public async Task UpdateUser(User user)
+        {
+            var existingUser = _userList.FirstOrDefault(u => u.Id == user.Id);
+            existingUser.FirstName = user.FirstName;
+            existingUser.LastName = user.LastName;
+            existingUser.Email = user.Email;
+            existingUser.Password = HashPassword(user.Password);
+        }
+
+        public void DeleteUser(int id)
+        {
+            var user = _userList.FirstOrDefault(u => u.Id == id);
+            _userList.Remove(user);
+        }
+    }
+}
